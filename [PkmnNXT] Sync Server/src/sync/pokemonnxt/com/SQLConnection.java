3934b29ceb56151b568722881888ff4c6444bd72
TheModerator <rljsowden@yahoo.co.uk>
2014-08-09 14:32:46 +0100

    Initial Commit.

---
+++ [PkmnNXT] Sync Server/src/sync/pokemonnxt/com/SQLConnection.java
@@ -0,0 +1,144 @@
+package sync.pokemonnxt.com;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import java.sql.Statement;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SQLConnection {
+	
+	String url = "jdbc:mysql://localhost:3306/";
+	String driver = "com.mysql.jdbc.Driver"; 
+	static String gameDBName = "NXT_GAME";
+	static String baseDBName = "NXT_BASE";
+	String gameDBUserName = "NXT_GAME-SYNC";
+	String gameDBPassword = "AqDErPDjjuUBzjcW";
+	Connection SQL;
+	Connection BaseSQL;
+	private Connection Temp;
+	SQLRenewTask SQLRenew;
+	Timer time = new Timer(); // Instantiate Timer Object
+	
+	public void ReconnectDatabases(){
+		try {
+		Class.forName(driver).newInstance(); 
+	  	SQL.close();
+	  	SQL =  DriverManager.getConnection(url+gameDBName,gameDBUserName,gameDBPassword); 
+	  	BaseSQL.close();
+	  	BaseSQL = DriverManager.getConnection(url+baseDBName,gameDBUserName,gameDBPassword);
+	  	} catch (SQLException | InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+	  		GlobalExceptionHandler GEH = new GlobalExceptionHandler();
+			GEH.uncaughtException(Thread.currentThread(), (Throwable) e);
+			e.printStackTrace();
+		} 
+	}
+	public void ConnectToSQLServer(){
+		try { 
+			System.out.println("[" + ConsoleColors.ANSI_GREEN + " WORKING " + ConsoleColors.ANSI_RESET + "]	Connecting SQL Server");
+		  	Class.forName(driver).newInstance(); 
+		  	SQL = DriverManager.getConnection(url+gameDBName,gameDBUserName,gameDBPassword); 
+		  	BaseSQL = DriverManager.getConnection(url+baseDBName,gameDBUserName,gameDBPassword); 
+		  	
+		  	SQLRenew = new SQLRenewTask(); // Instantiate SheduledTask class
+			time.schedule(SQLRenew, 50000, 600000); // Create Repetitively task for every 1 secs
+		  	System.out.println("SQL SERVER CONNECTION						[" + ConsoleColors.ANSI_GREEN + "   OK   " + ConsoleColors.ANSI_RESET + "]");
+	  	} catch (Exception e) { 
+	  		GlobalExceptionHandler GEH = new GlobalExceptionHandler();
+			GEH.uncaughtException(Thread.currentThread(), (Throwable) e);
+	  		System.out.println("SQL SERVER CONNECTION						[" + ConsoleColors.ANSI_YELLOW + "  FAIL  " + ConsoleColors.ANSI_RESET + "]");
+	  	} 
+	         
+}
+	public int Update(String query){
+		Statement st;
+		int RS;
+		try {
+			st = SQL.createStatement();
+			RS = st.executeUpdate(query);
+		} catch (SQLException e) {
+			GlobalExceptionHandler GEH = new GlobalExceptionHandler();
+			GEH.uncaughtException(Thread.currentThread(), (Throwable) e,"Error whilst attempting to run the following query: " + query);
+			RS = -1;
+		}
+		
+return RS;
+	}
+	public ResultSet Query(String query){
+		Statement st;
+		ResultSet RS;
+		try {
+			st = SQL.createStatement();
+			RS = st.executeQuery(query);
+		} catch (SQLException e) {
+			GlobalExceptionHandler GEH = new GlobalExceptionHandler();
+			GEH.uncaughtException(Thread.currentThread(), (Throwable) e, "Error whilst attempting to run the following query: " + query);
+			RS = null;
+		}
+		
+return RS;
+	}
+	  public int getInt(String query) {
+		  ResultSet RS;
+		     
+		    try {
+		    	PreparedStatement pstmt = SQL.prepareStatement(query);
+		      RS = pstmt.executeQuery();
+		      if (RS.next()) {
+		        return RS.getInt(1);
+		      } else {
+		        return -1;
+		      }
+		    } catch (Exception e) {
+				GlobalExceptionHandler GEH = new GlobalExceptionHandler();
+				GEH.uncaughtException(Thread.currentThread(), (Throwable) e,"Error whilst attempting to run the following query: " + query);
+		      return -1;
+		    }
+		  }
+	public ResultSet QueryBase(String query){
+		Statement st;
+		ResultSet RS;
+		try {
+			st = BaseSQL.createStatement();
+			RS = st.executeQuery(query);
+		} catch (SQLException e) {
+			GlobalExceptionHandler GEH = new GlobalExceptionHandler();
+			GEH.uncaughtException(Thread.currentThread(), (Throwable) e,"Error whilst attempting to run the following query: " + query);
+			RS = null;
+		}
+		
+return RS;
+	}
+
+public void DisconnectSQLServer(){
+	System.out.println("Disconnecting from SQL server...");
+	try {
+		SQLRenew.cancel();
+		System.out.println("[" + ConsoleColors.ANSI_GREEN + " WORKING " + ConsoleColors.ANSI_RESET + "]	Disconnecting SQL Server");
+		SQL.close();
+		BaseSQL.close();
+		System.out.println("SQL SERVER DISCONNECTION								[" + ConsoleColors.ANSI_GREEN + "   OK   " + ConsoleColors.ANSI_RESET + "]");
+	} catch (SQLException e) {
+		System.out.println("SQL SERVER DISCONNECTION								[" + ConsoleColors.ANSI_YELLOW + "  FAIL  " + ConsoleColors.ANSI_RESET + "]	");
+	} 
+	
+}
+
+//Create a class extends with TimerTask
+public class SQLRenewTask extends TimerTask {
+
+
+	// Add your task here
+	public void run() {
+		System.out.println("Renewing server connection...");
+		ReconnectDatabases();
+		System.out.println("Server connection renewed.");
+	}
+}
+
+}
---
+++ [PkmnNXT] Sync Server/src/sync/pokemonnxt/com/ThreadMonitor.java
@@ -0,0 +1,54 @@
+package sync.pokemonnxt.com;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
+import java.util.HashMap;
+
+import com.google.gson.annotations.Expose;
+
+public class ThreadMonitor extends Thread{
+	Boolean shutdown = false;
+	public static HashMap<Long,ThreadUsage> UsageMatrix = new HashMap<Long,ThreadUsage>();
+	public void run(){
+		 Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
+		ThreadMXBean tmxb = ManagementFactory.getThreadMXBean();
+		while(shutdown == false){
+			try {
+				sleep(5000);
+			for(Client c : MainServer.Clients){
+				if(c == null) continue;
+				if(System.currentTimeMillis() - c.lastRX > ServerVars.Timeout) {
+					c.timeOut();
+					if (c.player != null) c.player.signOut();
+					c.forceClose();
+					
+				}
+				
+				long TID = c.getId();
+				ThreadUsage TU = null;
+				if(UsageMatrix.containsKey(TID)) TU = UsageMatrix.get(TID);
+				if(TU == null) TU = new ThreadUsage();
+				TU.CPU =  (tmxb.getThreadCpuTime(TID) / 1000000)/(System.currentTimeMillis() - c.startTime);
+				TU.TID = TID;
+				TU.STATE = tmxb.getThreadInfo(TID).getThreadState();
+				TU.GTID = -1;
+				if(c.player != null) TU.GTID = c.player.GTID;
+				c.Performance = TU;
+				if(!UsageMatrix.containsKey(TID))  UsageMatrix.put(TID,TU);
+				
+			}
+			} catch (Exception e) {
+				GlobalExceptionHandler GEH = new GlobalExceptionHandler();
+				GEH.uncaughtException(Thread.currentThread(), (Throwable) e);
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	public class ThreadUsage{
+		@Expose double TID = -1;
+		@Expose int GTID = -1;
+		@Expose long CPU = -1;
+		@Expose State STATE = State.NEW;
+	}
+}
